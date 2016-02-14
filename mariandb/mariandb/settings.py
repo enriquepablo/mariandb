ad98bb4e2e6594f4e51ca620a29ebea76345450f
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-02-14 16:12:44 +0100

    local settings, pg

--- mariandb/mariandb/settings.py
+++ mariandb/mariandb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@lc@z0zd8(lx4k)@eao3)=627&(0juc%8b#aong)eq-v=21)-h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
